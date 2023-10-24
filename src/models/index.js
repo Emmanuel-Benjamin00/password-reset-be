26361057c6f3ac99914467ed2f18dd7dd8e9bd96
Emmanuel Benjamin D <emmanuel26112000@gmail.com>
2023-10-24 15:01:33 +0530

    removed some unnecessary consoles

--- src/models/index.js
+++ src/models/index.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 try {
-    console.log(process.env.dbUrl, process.env.dbName)
     mongoose.connect(`${process.env.dbUrl}/${process.env.dbName}`)
 } catch (error) {
     console.log(error)
